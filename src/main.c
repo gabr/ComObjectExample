88d16a3f27638e239455d94f1bca473b65795c85
Arkadiusz Gabrys <arkadiuszgabrys@gmail.com>
2017-08-02 10:36:41 +0200

    Guids

--- src/main.c
+++ src/main.c
@@ -5,13 +5,24 @@
 
 #define BUFFER_LEN 80
 
+// GUID Generator: C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\guidgen.exe
+
+// for IExample
+// {86461787-A54D-42A6-A126-4310B476B3DE}
+DEFINE_GUID(CLSID_IExample, 0x86461787, 0xa54d, 0x42a6, 0xa1, 0x26, 0x43, 0x10, 0xb4, 0x76, 0xb3, 0xde);
+
+// for IExampleVtbl
+// {08C8DC7A-ED70-4FC1-BDF4-F0F930FBAD63}
+DEFINE_GUID(IID_IExample, 0x8c8dc7a, 0xed70, 0x4fc1, 0xbd, 0xf4, 0xf0, 0xf9, 0x30, 0xfb, 0xad, 0x63);
+
+
 typedef struct iExample IExample;
 typedef HRESULT STDMETHODCALLTYPE QueryInterfacePtr(IExample *, REFIID, void **);
 typedef ULONG STDMETHODCALLTYPE AddRefPtr(IExample *);
 typedef ULONG STDMETHODCALLTYPE ReleasePtr(IExample *);
 
-typedef long SetStringPtr(IExample *, char *);
-typedef long GetStringPtr(IExample *, char *, long);
+typedef HRESULT STDMETHODCALLTYPE SetStringPtr(IExample *, char *);
+typedef HRESULT STDMETHODCALLTYPE GetStringPtr(IExample *, char *, long);
 
 typedef struct iExampleVtbl
 {
@@ -25,12 +36,13 @@ typedef struct iExampleVtbl
 
 typedef struct iExample
 {
+  // first pointer has to be pointer to VTable
   IExampleVtbl *lpVtbl;
   DWORD         count;
   char          buffer[BUFFER_LEN];
 } IExample;
 
-long SetString(IExample *this, char *str)
+HRESULT STDMETHODCALLTYPE SetString(IExample *this, char *str)
 {
   DWORD i;
 
@@ -42,7 +54,7 @@ long SetString(IExample *this, char *str)
   return 0;
 }
 
-long GetString(IExample *this, char *buffer, DWORD length)
+HRESULT STDMETHODCALLTYPE GetString(IExample *this, char *buffer, DWORD length)
 {
   DWORD i;
 
@@ -58,6 +70,14 @@ long GetString(IExample *this, char *buffer, DWORD length)
 
 int main()
 {
+  IExample *example = (IExample *)GlobalAlloc(GMEM_FIXED, sizeof(IExample));
+
+  strcpy_s(example->buffer, sizeof example->buffer, "Hello World!");
+  example->count = lstrlen(example->buffer);
+
+  printf("Count: %d\n%s", example->count, example->buffer);
+
+  GlobalFree(example);
   return 0;
 }
 
